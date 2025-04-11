520416d65a5a9ac1f07f40c5b03a83a175d51848
Madhur Gaur <madhurgaur9140@gmail.com>
2025-04-11 14:46:22 +0530

    clerk solved

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,28 @@ const nextConfig = {
       bodySizeLimit: "5mb",
     },
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              font-src 'self' data: https://fonts.gstatic.com https://*.clerk.dev;
+              script-src 'self' https://*.clerk.dev 'unsafe-inline';
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.clerk.dev;
+              img-src 'self' data: https://*.clerk.dev https://randomuser.me;
+              connect-src 'self' https://*.clerk.dev https://*.clerk.com;
+              frame-src https://*.clerk.dev https://*.clerk.com;
+            `.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
