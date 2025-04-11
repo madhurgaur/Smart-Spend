6a519aa48daca5d94d80a481755294e1194cbc1c
Madhur Gaur <madhurgaur9140@gmail.com>
2025-04-11 13:15:31 +0530

    trnsc

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,5 +1,23 @@
-import AddTransactionClient from "./addtransaction";
+import { defaultCategories } from "@/data/categories";
+import { getUserAccounts } from "@/actions/dashboard";
+import { getTransaction } from "@/actions/transaction";
+import AddTransactionClient from "./client";
 
-export default function AddTransactionPage() {
-  return <AddTransactionClient />;
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
+
+  const editId = searchParams?.edit || null;
+  let initialData = null;
+
+  if (editId) {
+    initialData = await getTransaction(editId);
+  }
+
+  return (
+    <AddTransactionClient
+      accounts={accounts}
+      initialData={initialData}
+      editId={editId}
+    />
+  );
 }
